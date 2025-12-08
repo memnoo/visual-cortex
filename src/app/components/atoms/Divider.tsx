5dd2d3d013dee6402c26f6f310ca22fd8b7382aa
QiMirda <admiquel94@gmail.com>
2025-12-08 15:44:57 +0900

    Divider component

---
+++ src/app/components/atoms/Divider.tsx
@@ -0,0 +1,15 @@
+type DividerProps = {
+  horizontal?: boolean;
+  vertical?: boolean;
+};
+
+export const Divider = ({
+  horizontal = true,
+  vertical = false,
+}: DividerProps) => {
+  if (vertical) {
+    return <div className="h-full mx-1 border-[1px]" />;
+  }
+
+  return horizontal && <hr className="w-full my-1" />;
+};

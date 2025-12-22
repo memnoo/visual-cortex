6404d51f58a840139c1fad5a23fed0f2a8d983b3
QiMirda <admiquel94@gmail.com>
2025-12-22 23:49:44 +0100

    i18n in dashboard

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,34 +1,33 @@
 "use client";
 
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 import { Button } from "@/app/components/atoms/Button";
 
 export default function DashboardPage() {
+  const t = useTranslations();
+
+  const links = [
+    { path: "/decks", label: t("nav.decks") },
+    { path: "/review-session", label: t("nav.review-session") },
+  ];
+
   return (
     <section className="flex flex-col gap-2 content-stretch w-full">
-      <Link href="/decks">
-        <Button
-          type="button"
-          variant="transparent"
-          iconName="chevron_right"
-          iconPosition="right"
-          className="w-full h-full text-left"
-        >
-          Go to decks
-        </Button>
-      </Link>
-      <Link href="/review-session">
-        <Button
-          type="button"
-          variant="transparent"
-          iconName="chevron_right"
-          iconPosition="right"
-          className="w-full h-full text-left"
-        >
-          Review decks
-        </Button>
-      </Link>
+      {links.map(({ path, label }, index) => (
+        <Link key={index} href={path}>
+          <Button
+            type="button"
+            variant="transparent"
+            iconName="chevron_right"
+            iconPosition="right"
+            className="w-full h-full text-left"
+          >
+            {label}
+          </Button>
+        </Link>
+      ))}
     </section>
   );
 }

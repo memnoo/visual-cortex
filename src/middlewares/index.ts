3a9c7b98ac40b490a4f717aa3c8402d0986adaf9
QiMirda <admiquel94@gmail.com>
2026-01-05 19:31:32 +0100

    Fix middlewares execution

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -4,8 +4,13 @@ import { middleware as authMiddleware } from "./auth";
 import { middleware as i18nMiddleware } from "./i18n";
 
 export async function runMiddlewares(request: NextRequest) {
-  await i18nMiddleware(request);
-  await authMiddleware(request);
+  let response: NextResponse | undefined;
+
+  response = await i18nMiddleware(request);
+  if (response && response.status !== 200) return response;
+
+  response = await authMiddleware(request);
+  if (response && response.status !== 200) return response;
 
   return NextResponse.next();
 }
